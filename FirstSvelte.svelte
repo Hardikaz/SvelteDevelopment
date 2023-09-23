3b1157f3d518d87d7a88e66ab7db20d1b487b00f
Hardik Sharma <95474096+Hardikaz@users.noreply.github.com>
2023-09-24 00:54:13 +0530

    Create FirstSvelte.svelte

---
+++ FirstSvelte.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name="Hardik Sharma";
+	let src="https://img.freepik.com/premium-vector/ninja-shinobi-position-fight-enemy-with-his-katana-hand-drawn-illustration_73644-31.jpg?w=2000";
+	let alt="image";
+
+	function print(){
+		alert("I am being clicked");
+	}
+</script>
+<h1>Good Morning {name.toUpperCase()}</h1>
+<button on:click={print}>Click Me</button>
+<img {src} {alt}/>
+<style>
+	h1{
+		color:blue
+	}
+</style>
+
+
+
+
