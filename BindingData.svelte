9a2d1bc969ce1cce2ec3d6ab2df833cf01c239fa
Hardik Sharma <95474096+Hardikaz@users.noreply.github.com>
2023-09-24 01:09:45 +0530

    Create BindingData.svelte

---
+++ BindingData.svelte
@@ -0,0 +1,9 @@
+<script>
+	let celcius=0;
+	$:  fahrenheit=celcius*1.8 +32;
+</script>
+
+<h1>Type in celcius</h1>
+<input type="number" bind:value={celcius}>
+
+<p>The temp in Fahrenheit is {fahrenheit}</p>
