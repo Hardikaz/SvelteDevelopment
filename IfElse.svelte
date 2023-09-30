49a09cbcb4b6a5e47f713f06f0bd7d05fb4d7c22
Hardik Sharma <95474096+Hardikaz@users.noreply.github.com>
2023-10-01 01:07:57 +0530

    Create IfElse.svelte

---
+++ IfElse.svelte
@@ -0,0 +1,17 @@
+<script>
+let count=0;	
+
+function add()
+	{
+		count+=1;
+	}
+</script>
+<button on:click={add}>Click me {count} times</button>
+{#if count > 10}
+	<p>Count is greater than 10</p>
+{:else if count==10}
+	<p>Count is equal to 10</p>
+
+	{:else}
+	<p>Count is less than 10</p>
+{/if}
