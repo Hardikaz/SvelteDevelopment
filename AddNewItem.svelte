af2860e05fb26c76f25cd8607d6deca9fff33305
Hardik Sharma <95474096+Hardikaz@users.noreply.github.com>
2023-10-01 00:48:50 +0530

    Create AddNewItem.svelte

---
+++ AddNewItem.svelte
@@ -0,0 +1,14 @@
+<script>
+	let fruit=["apples","oranges"];
+
+	function addItem()
+		{
+			// fruit.push("lemons");
+			// fruit=fruit;
+
+			fruit=[...fruit,"Lemon"];
+		}
+	$: fruitsList=fruit.join(', ');
+</script>
+<h1>Available fruits: {fruitsList}</h1>
+<button on:click={addItem}>Add Item</button>
